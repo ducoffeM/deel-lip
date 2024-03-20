638e0b5b2899ef64848d5ce93b160eb32e71ebe7
ducoffeM <melanie.ducoffe@airbus.com>
2024-03-20 19:56:01 +0100

    remove tensorflow.keras depency from test_initializers

--- tests/test_initializers.py
+++ tests/test_initializers.py
@@ -5,10 +5,10 @@
 import unittest
 import numpy as np
 import tensorflow as tf
-from tensorflow.keras import Sequential
-from tensorflow.keras import backend as K, metrics
-from tensorflow.keras.layers import Dense
-from tensorflow.keras.optimizers import Adam
+from keras import Sequential
+from keras import backend as K, metrics
+from keras.layers import Dense
+from keras.optimizers import Adam
 from deel.lip.initializers import SpectralInitializer
 
 FIT = "fit_generator" if tf.__version__.startswith("2.0") else "fit"
